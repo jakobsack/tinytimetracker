a2a1e9dad6c75619f4d675f02932ec0dc4e002df
Jakob Sack <mail@jakobsack.de>
2016-04-24 16:08:02 +0200

    Remove foreign keys in migrations

--- db/migrate/20160423150922_create_projects.rb
+++ db/migrate/20160423150922_create_projects.rb
@@ -1,10 +1,10 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
-      t.references :user, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: false
       t.string :name
       t.string :color
-      t.references :parent, index: true, foreign_key: true
+      t.references :parent, index: true, foreign_key: false
       t.boolean :active
 
       t.timestamps null: false
--- db/migrate/20160423151046_create_records.rb
+++ db/migrate/20160423151046_create_records.rb
@@ -1,8 +1,8 @@
 class CreateRecords < ActiveRecord::Migration
   def change
     create_table :records do |t|
-      t.references :user, index: true, foreign_key: true
-      t.references :project, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: false
+      t.references :project, index: true, foreign_key: false
       t.datetime :begun_at
       t.datetime :ended_at
 
